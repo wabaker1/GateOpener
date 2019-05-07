ca4b8fb00b092cc4f3e8d94888dc9c045f18803e
unknown <jjgilbe3@asu.edu>
2019-05-07 12:38:34 -0700

    Adding current (not working) code for the native activity, trying to search for devices to connect to via bluetooth

---
+++ GateOpenerProject/app/src/main/java/com/gateopenerproject/Constants.java
@@ -0,0 +1,12 @@
+package com.gateopenerproject;
+
+/**
+ * Created by Jessica Gilbert on 12/30/2018.
+ */
+
+public class Constants {
+    public static final String TAG = "";
+    public static final String FIND = "Scan";
+    public static final String STOP_SCANNING = "Stop Scanning";
+    public static final String SCANNING = "Scanning";
+}
--- GateOpenerProject/app/src/main/java/com/gateopenerproject/NativeConnectActivity.java
+++ GateOpenerProject/app/src/main/java/com/gateopenerproject/NativeConnectActivity.java
@@ -1,10 +1,46 @@
 package com.gateopenerproject;
 
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NativeConnectActivity extends AppCompatActivity {
+    private BluetoothLeScanner scanner = null;
+    private BluetoothAdapter bluetooth_adapter = null;
+    private Context context1;
 
+    public NativeConnectActivity(){
+        Context context;
+        context = context1;
+
+        final BluetoothManager bluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetooth_adapter = bluetoothManager.getAdapter();
+
+        // check bluetooth is available and on
+        if (bluetooth_adapter == null || !bluetooth_adapter.isEnabled()) {
+            Log.d(Constants.TAG, "Bluetooth is NOT switched on");
+            Intent enableBtIntent = new Intent( BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            enableBtIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(enableBtIntent);
+        }
+        else
+            Log.d(Constants.TAG, "Bluetooth is switched on");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
